37d2c16da0345ad262d675344d33a9bb6a124bb8
mingww64 <me@lcia.eu.org>
2026-04-10 17:02:17 -0700

    add lan-mouse service init condition

--- modules/services/lan-mouse.nix
+++ modules/services/lan-mouse.nix
@@ -3,18 +3,20 @@
 {
   # Define your service (ircSession in this case)
   systemd.services.lan-mouse = {
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    description = "Start the lan-mouse client of felicia.";
+    after = [ "network.target" "graphical-session.target" ];
+    description = "lan-mouse client of felicia.";
     serviceConfig = {
       Type = "simple";
       User = "felicia";
-      ExecStart = " ${pkgs.bash}/bin/bash -c 'XDG_RUNTIME_DIR=/run/user/1000 ${pkgs.lan-mouse}/bin/lan-mouse -d'";
+      Environment = "XDG_RUNTIME_DIR=/run/user/1000";
+      ExecStart = "${pkgs.lan-mouse}/bin/lan-mouse -d";
+      Restart = "on-failure";
+      RestartSec = "5s";
     };
+    unitConfig.ConditionUser = "felicia";
   };
 
   # Install necessary packages (lan-mouse in this case)
   environment.systemPackages = [ pkgs.lan-mouse ];
 
 }
-
